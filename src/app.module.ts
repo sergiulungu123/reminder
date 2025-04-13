4452d18d855b476baf15392580ac672e0c8ed936
Sergiu Lungu <sergiu.lungu@opentech.email>
2025-04-13 10:38:43 +0300

    add bot tg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ReminderService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    // Инициализируем модуль Telegram с помощью токена из переменных окружения
+    TelegrafModule.forRoot({
+      token: '7557518463:AAHbMvDOFwDleMQ8zpzcHnn2yAN0EaF1fr0',
+    }),
+    // Инициализируем планировщик задач NestJS
+    ScheduleModule.forRoot(),
+  ],
+  providers: [ReminderService],
 })
 export class AppModule {}

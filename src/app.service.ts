03310ec2d2c2830b14a12a94ffef97127652e7c9
Sergiu Lungu <sergiu.lungu@opentech.email>
2025-04-13 10:49:17 +0300

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@nestjs/common';
 // import { Cron, CronExpression } from '@nestjs/schedule';
-// import { Ctx, InjectBot } from 'nestjs-telegraf';
-// import { Context, Telegraf } from 'telegraf';
+import { Ctx, InjectBot } from 'nestjs-telegraf';
+import { Context, Telegraf } from 'telegraf';
 
 @Injectable()
-export class ReminderService {}
+export class ReminderService {
+  constructor(
+    // Получаем инстанс Telegram-бота для отправки сообщений
+    @InjectBot() private readonly bot: Telegraf<Context>,
+  ) {}
+
+  // Пример: отправка напоминания каждые 10 секунд (для демонстрационных целей)
+  // @Cron(CronExpression.EVERY_10_SECONDS)
+  async handleCron() {
+    console.log('Напоминание');
+    const chatId = '434811237';
+    if (chatId) {
+      await this.bot.telegram.sendMessage(
+        chatId,
+        'Напоменаю: что ты прекрасна:)',
+      );
+    }
+  }
+
+  async handleStart(@Ctx() ctx: Context) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const chatId = ctx?.chat?.id;
+    await ctx.reply(`Ваш chat-id: ${chatId}`);
+  }
+}

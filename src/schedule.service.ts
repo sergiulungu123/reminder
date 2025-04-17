549b5e030ca4fcb86bd3f586cde5e362d3f91708
Sergiu Lungu <sergiu.lungu@opentech.email>
2025-04-17 16:47:29 +0300

    add log

--- src/schedule.service.ts
+++ src/schedule.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { SupabaseService } from './supabase.service';
 import { InjectBot } from 'nestjs-telegraf';
@@ -6,6 +6,7 @@ import { Telegraf } from 'telegraf';
 
 @Injectable()
 export class ReminderSchedulerService {
+  private readonly logger = new Logger(ReminderSchedulerService.name);
   constructor(
     private readonly supabase: SupabaseService,
     @InjectBot() private readonly bot: Telegraf<any>, // ✅ вот это главное
@@ -15,6 +16,8 @@ export class ReminderSchedulerService {
 
   @Cron(CronExpression.EVERY_MINUTE)
   async handleReminderCheck() {
+    const serverDate = new Date().toISOString();
+    this.logger.debug(`Server date: ${serverDate}`);
     const reminders = await this.supabase.getDueReminders();
 
     for (const reminder of reminders) {
